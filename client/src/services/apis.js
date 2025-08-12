e4480fd82981c73df9b0ed942dea5ef447937b2c
Anshuman7080 <164294129+Anshuman7080@users.noreply.github.com>
2025-08-12 19:14:52 +0530

    Update apis.js

--- client/src/services/apis.js
+++ client/src/services/apis.js
@@ -1,7 +1,7 @@
 
 // const BASE_URL=process.env.REACT_APP_BASE_URL;
 
-const BASE_URL="https://chat-app-backend-a3qu.onrender.com/api/v1";
+const BASE_URL="https://chat-app-backend-lw56.onrender.com/api/v1";
 console.log(BASE_URL);
 
 export const endpoints={
