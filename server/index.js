5d02f33d4325fa4277abb9add6c076b8cf38089b
Anshuman7080 <164294129+Anshuman7080@users.noreply.github.com>
2025-08-12 19:16:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,14 +9,14 @@ const cookieParser = require("cookie-parser");
 const fileUpload = require('express-fileupload');
 const {app,server}=require("./socket/index")
 
-// app.use(cors({
-//     origin:"https://chat-app-frontend-q1og.onrender.com"
-//     credentials:true,
-// }));
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin:"https://chat-app-frontend-q1og.onrender.com"
     credentials:true,
 }));
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:3000",
+//     credentials:true,
+// }));
 const PORT=process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
