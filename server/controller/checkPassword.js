199d32dda8b505fd63076608bbc1b4dff8c4850c
Anshuman7080 <164294129+Anshuman7080@users.noreply.github.com>
2025-08-12 19:51:40 +0530

    Update checkPassword.js

--- server/controller/checkPassword.js
+++ server/controller/checkPassword.js
@@ -42,8 +42,8 @@ const tokenData={
 
         const cookieOptions = {
             httpOnly: true,
-             sameSite: "None",
-            secure: true,
+            sameSite: "Lax",   
+  secure: false, 
              maxAge: 30 * 24 * 60 * 60 * 1000 
         }
 
